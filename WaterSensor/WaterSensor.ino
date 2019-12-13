1421d7f895b8e5998d91428cb8c4095a62259738
momischmiddi <Moritzschmidt1995@gmail.com>
2019-12-13 19:47:22 +0100

    Arduino fertig implementiert

--- WaterSensor/WaterSensor.ino
+++ WaterSensor/WaterSensor.ino
@@ -1,9 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "AndroidAPAF67";
-const char* password = "ioei9326";
+void connect();
 
+// WeMos D1 R2 & mini
+
+const char* ssid = "SchnauzePuppe";
+const char* password = "78131887303241935007";
+
+//const char* ssid = "AndroidAPAF67";
+//const char* password = "ioei9326";
+ 
 void setup () {
   Serial.begin(115200);
   Serial.println("Started.");
@@ -13,7 +20,50 @@ void setup () {
 }
  
 void loop() {
-  int value = analogRead(A0);
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("####################################");
+    Serial.println("Connected");
+
+    HTTPClient http;
+     
+    http.begin("http://smartplant.azurewebsites.net/updateplant");
+    http.addHeader("Content-Type", "application/json");
+
+    int value = analogRead(A0);
+    String mac = WiFi.macAddress();
+
+    String payload;
+
+    payload += "{\"macAddress\":\"";
+    payload +=  mac;
+    payload += "\",\"watervalue\":\"";
+    payload += String(value);
+    payload += "\",\"owner\":\"";
+    payload += "Momo";
+    payload += "\",\"planttype\":\"";
+    payload += String(0);
+    payload += "\"}";
+    
+    Serial.println(payload);
+        
+    int httpCode = http.sendRequest("PUT", payload);
+     
+    if (httpCode > 0) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      Serial.println(httpCode);
+    }
+
+    Serial.println("####################################");
+    Serial.println("");
+   
+    http.end();
+    delay(1000);
+  } else {
+    Serial.println("Connection lost. Reconnecting.");
+    connect();
+  }
 }
 
 void connect()
@@ -22,6 +72,5 @@ void connect()
     delay(1000);
     Serial.println("Connecting..");
   }
-
-  Serial.println("Connected.");
 }
+
